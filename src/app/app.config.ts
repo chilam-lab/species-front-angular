e541ec405c29247c99d40e6110bfe4df3b170278
Juan Carlos Salazar <jsalazar@MacBookPro-jsalazar.local>
2026-02-24 11:17:44 -0600

    ajustes para carga de selector y navegaor taxonomico

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; 
 import { API_BASE_URL } from 'taxon-shared';
 import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
+console.log('TOKEN API_BASE_URL:', API_BASE_URL);
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
     { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
   ]
 };
